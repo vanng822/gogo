e1811331e0eb0233318bf82d510c4cfba907cd83
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2014-09-11 21:34:08 +0200

    testing gpsutil

---
+++ src/alla_vagar_leder_till_rome.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/vanng822/gpsutil"
+	"fmt"
+	"math/rand"
+	"flag"
+)
+
+func main() {
+	var k, p int
+	flag.IntVar(&k, "k", 10, "Number of km")
+	flag.IntVar(&p, "p", 4, "Number of points per km")
+	flag.Parse()
+	for i := 1; i <= k; i++ {
+		for j := 0; j < p; j++ {			
+			fmt.Println(i, gpsutil.GetPointByBearing(41.8723889, 12.48018019999995, float64(rand.Intn(360)), float64(i) * 1000))
+		}
+	}
+}
